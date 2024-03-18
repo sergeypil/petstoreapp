4f43d7bfd16d3c77846217625913a0b72f8ae4c6
Sergey Pil <realsergeypil@gmail.com>
2024-03-19 00:59:49 +0500

    Add attach()

--- src/main/java/com/chtrembl/petstoreapp/PetstoreappApplication.java
+++ src/main/java/com/chtrembl/petstoreapp/PetstoreappApplication.java
@@ -1,5 +1,6 @@
 package com.chtrembl.petstoreapp;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -14,6 +15,7 @@ public class PetstoreappApplication {
 	private static Logger logger = LoggerFactory.getLogger(PetstoreappApplication.class);
 
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
 		SpringApplication.run(PetstoreappApplication.class, args);
 		logger.info("PetStoreApp started up... " + System.getProperty("catalina.base"));
 		try {
